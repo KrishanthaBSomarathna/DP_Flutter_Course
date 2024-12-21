c73b9282359c2b33fcec5813818574156307624e
Krishantha Bandara <info.krishantha.b@gmail.com>
2024-12-21 15:57:28 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 187, 1, 1),
-          leading: const Icon(
-            Icons.menu,
-            size: 24.0,
-            color: Colors.white,
-          ),
-          actions: const [
-            Icon(
-              Icons.search,
-              size: 24.0,
-              color: Colors.white,
-            )
-          ],
-          title: const Text(
-            'My First App',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        body: const Center( 
-          child: Text("Hello Friends this is my first flutter APP"),
-        ),
-      ),
-    );
+    return MaterialApp();
   }
 }
