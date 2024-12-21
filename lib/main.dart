8d5b73a2fef1f18c915503270889cfc00e0e7a74
Krishantha Bandara <info.krishantha.b@gmail.com>
2024-12-21 18:00:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Container",
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            width: 200.0,
+            height: 200.0,
+            margin: EdgeInsets.all(50.0),
+            
+            decoration:BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.blue,
+              border: Border.all(
+                width: 3.0,
+                color: Colors.red,
+              )
+            ),
+            child: const Text('Flutter'),
+          ),
+        ),
+      ),
+    );
   }
 }
