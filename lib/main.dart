3c9327101521d708b1c1e435a5393c1926d31319
Krishantha Bandara <info.krishantha.b@gmail.com>
2024-12-24 20:13:12 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      locale: DevicePreview.locale(context), // Set locale to the Device Preview locale
+      locale: DevicePreview.locale(
+          context), // Set locale to the Device Preview locale
       builder: DevicePreview.appBuilder, // Wrap the app builder
       home: Scaffold(
-      
+        appBar: AppBar(
+          backgroundColor: Color(0xff9E00FF),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 175.0,
+                color: Color(0xff06FFA5),
+              ),
+              Container(
+                width: double.infinity,
+                height: 175.0,
+                color: Color(0xff06FFA5),
+              ),
+              Row( 
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 175.0,
+                    height: 350.0,
+                    color: Color(0xffFFE500),
+                  ),
+                  Container(
+                    width: 175.0,
+                    height: 350.0,
+                    color: Color(0xffFFE500),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
