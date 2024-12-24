ca2216aa22ad45d49befc6ebf1f9b4ae103dd990
Krishantha Bandara <info.krishantha.b@gmail.com>
2024-12-24 19:35:06 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xff9E00FF),
+        ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,  
+          children: [
+            Container(
+              color: Color(0xff06FFA5),
+              width: double.infinity,
+              height: 350.0,
+            ),
+            Container(
+              color: Color(0xffFFE500),
+              width: double.infinity,
+              height: 350.0,
+            )
+          ],
+        ),
+      ),
+      ),
+      
+    );
   }
 }
