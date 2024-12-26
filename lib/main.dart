0d8fce71f8130b5da1facff71424f701a1618d2d
Krishantha Bandara <info.krishantha.b@gmail.com>
2024-12-26 22:58:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      locale: DevicePreview.locale(context), // Set locale to the Device Preview locale
+      locale: DevicePreview.locale(
+          context), // Set locale to the Device Preview locale
       builder: DevicePreview.appBuilder, // Wrap the app builder
       home: Scaffold(
-      
+        appBar: AppBar(
+          backgroundColor: Color(0xff9E00FF),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 20.0,
+                ),
+                color: Color(0xff85FFD3),
+                height: 350.0,
+                width: double.infinity,
+                child: const Text(
+                  'Flutter is an open-source UI (User Interface) software development kit created by Google. It is used to build natively compiled applications for mobile, web, and desktop from a single codebase. Flutter was first introduced in 2015.',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 20.0,
+                ),
+                color: Color(0xffFFE500),
+                height: 350.0,
+                width: double.infinity,
+                child: const Text(
+                  'Flutter is an open-source UI (User Interface) software development kit created by Google. It is used to build natively compiled applications for mobile, web, and desktop from a single codebase. Flutter was first introduced in 2015.',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
